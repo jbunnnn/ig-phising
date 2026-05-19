3b42d0d9e5db02922d3c4ea86ad29575cd762c3d
ressaeka <reysaeka10@gmail.com>
2026-05-20 01:08:29 +0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-import { closeDatabase } from './database/query.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const HOST = process.env.HOST || (
+  process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost"
+);
+
+const server = app.listen(PORT,HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
 });
